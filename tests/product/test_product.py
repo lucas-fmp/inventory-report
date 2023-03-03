e26415d6a5223f26ec72aece59275462ea9c8b67
Lucas Fernando Macedo Pereira <lucas@pop-os.localdomain>
2023-03-03 15:38:41 -0300

    requisito 1 - teste do construtor do objeto Produto

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,39 @@
 # from inventory_report.inventory.product import Product
 
 
+from inventory_report.inventory.product import Product
+
+
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    fake_product = {
+        "id": "1",
+        "nome_do_produto": "Nicotine Polacrilex",
+        "nome_da_empresa": "Target Corporation",
+        "data_de_fabricacao": "2021-02-18",
+        "data_de_validade": "2023-09-17",
+        "numero_de_serie": "CR25 1551 4467 2549 4402 1",
+        "instrucoes_de_armazenamento": "instrucao 1",
+    }
+
+    product_data = Product(
+        fake_product["id"],
+        fake_product["nome_do_produto"],
+        fake_product["nome_da_empresa"],
+        fake_product["data_de_fabricacao"],
+        fake_product["data_de_validade"],
+        fake_product["numero_de_serie"],
+        fake_product["instrucoes_de_armazenamento"],
+    )
+
+    assert product_data.id == fake_product["id"]
+    assert product_data.nome_do_produto == fake_product["nome_do_produto"]
+    assert product_data.nome_da_empresa == fake_product["nome_da_empresa"]
+    assert (
+        product_data.data_de_fabricacao == fake_product["data_de_fabricacao"]
+    )
+    assert product_data.data_de_validade == fake_product["data_de_validade"]
+    assert product_data.numero_de_serie == fake_product["numero_de_serie"]
+    assert (
+        product_data.instrucoes_de_armazenamento
+        == fake_product["instrucoes_de_armazenamento"]
+    )
