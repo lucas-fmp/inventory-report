9cc48053307384aa9da89f8ebde8196723686924
Lucas Fernando Macedo Pereira <lucas@pop-os.localdomain>
2023-03-03 16:04:04 -0300

    requisito 2 - gera versão simplificada do relatório

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,47 @@
+import datetime
+
+
 class SimpleReport:
-    pass
+    @staticmethod
+    def generate(product_list):
+
+        oldest_date = min(
+            [
+                datetime.datetime.strptime(
+                    product["data_de_fabricacao"], "%Y-%m-%d"
+                )
+                for product in product_list
+            ]
+        ).strftime("%Y-%m-%d")
+
+        now = datetime.datetime.now()
+
+        valid_products = [
+            product
+            for product in product_list
+            if datetime.datetime.strptime(
+                product["data_de_validade"], "%Y-%m-%d"
+            )
+            > now
+        ]
+
+        closest_valid_date = min(
+            [
+                datetime.datetime.strptime(
+                    product["data_de_validade"], "%Y-%m-%d"
+                )
+                for product in valid_products
+            ]
+        ).strftime("%Y-%m-%d")
+
+        companies = [product["nome_da_empresa"] for product in product_list]
+        company_count = {
+            company: companies.count(company) for company in set(companies)
+        }
+        most_products_company = max(company_count, key=company_count.get)
+
+        report = f"Data de fabricação mais antiga: {oldest_date}\n"
+        report += f"Data de validade mais próxima: {closest_valid_date}\n"
+        report += f"Empresa com mais produtos: {most_products_company}\n"
+
+        return report
